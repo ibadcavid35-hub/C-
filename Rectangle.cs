36dbb01c75cc0d8d9f95396ff3df1e37029104bf
Javid Ibad <ibadcavid35@gmail.com>
2026-06-15 13:33:48 +0400

    fiveth commit

---
+++ Rectangle.cs
@@ -0,0 +1,13 @@
+﻿using Shapede;
+namespace ConsoleApp8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Square squ = new Square(5);
+            squ.Height = 10;
+            Console.WriteLine(squ);
+        }
+    }
+}
